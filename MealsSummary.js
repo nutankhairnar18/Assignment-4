e694373dc5bf28e05e5298435cbb69530bfbe24d
nutankhairnar18 <141702505+nutankhairnar18@users.noreply.github.com>
2023-08-22 00:51:45 +0530

    Add files via upload

---
+++ MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Your Favorite Food delivery Partner</h2>
+      <p>
+      You select, we deliver- fast, fresh, and instant food.
+      </p>
+      <p>
+      Our cutting-edge Food Delivery Slogans Generator is here to serves a delectable menu of catchy phrases that will excite your taste buds.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
